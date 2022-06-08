3a0f0ae922630eef6134a880d8124ef029d27ff0
neha sabila <nehasabila62@gmail.com>
2022-06-08 22:23:09 +0700

    aa

--- resources/views/layouts/usersidebar.blade.php
+++ resources/views/layouts/usersidebar.blade.php
@@ -5,7 +5,7 @@
     <ul class="sidebar-nav" id="sidebar-nav">
     
       <li class="nav-item">
-        <a class="nav-link" href="/user/dashboard">
+        <a class="nav-link collapsed" href="/user/dashboard">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
@@ -28,7 +28,7 @@
         </a>
       </li>
      @endif
-
+    
       <li class="nav-item">
         <a class="nav-link collapsed" href="/user/kategori">
         <i class="bi bi-menu-button-wide"></i>
@@ -42,20 +42,6 @@
           <span>Komentar</span>
         </a>
       </li>
-    
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-menu-button-wide"></i><span>Components</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="components-badges.html">
-              <i class="bi bi-circle"></i><span>Badges</span>
-            </a>
-          </li>
-        
-        </ul>
-      </li><!-- End Components Nav -->
     
     </ul>
     
