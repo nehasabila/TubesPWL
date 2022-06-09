1acbb8d525cee5cdb9206b4bbd69ca706362eefe
Jo766hi <jonatansilalahi09@gmail.com>
2022-06-09 22:03:30 +0700

    Edit

--- resources/views/layouts/blogapp.blade.php
+++ resources/views/layouts/blogapp.blade.php
@@ -50,28 +50,7 @@
         <h1>Blog</h1>
       </a>
 
-      <nav id="navbar" class="navbar">
-        <ul>
-          <li><a href="/">Blog</a></li>
-          {{-- <li><a href="single-post.html">Single Post</a></li> --}}
-          {{-- <li class="dropdown"><a href="#"><span>Categories</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-            <ul>
-              <li><a href="search-result.html">Search Result</a></li>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li> --}}
-
-          <li><a href="about.html">About</a></li>
-          <li><a href="contact.html">Contact</a></li>
-
-          
-        </ul>
-      </nav><!-- .navbar -->
+      
 
       <div class="position-relative">
 
